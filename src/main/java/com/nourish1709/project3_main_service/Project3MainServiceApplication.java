fb1d0415a4c4e4cd3ae0f0c5fa80df88c071ef29
zhenia <zhenya.savonenko944@gmail.com>
2021-02-22 19:50:27 +0200

    Initial commit

---
+++ src/main/java/com/nourish1709/project3_main_service/Project3MainServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nourish1709.project3_main_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3MainServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project3MainServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nourish1709/project3_main_service/Project3MainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nourish1709.project3_main_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3MainServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
