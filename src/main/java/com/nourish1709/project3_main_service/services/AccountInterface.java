957197124aa45c96058aea12fc5684590145bb94
AndriianaMazan <64812682+AndriianaMazan@users.noreply.github.com>
2021-02-24 16:24:34 +0200

    Created AccountRepository

--- src/main/java/com/nourish1709/project3_main_service/services/AccountInterface.java
+++ src/main/java/com/nourish1709/project3_main_service/services/AccountInterface.java
@@ -1,11 +1,21 @@
 package com.nourish1709.project3_main_service.services;
 
-import com.nourish1709.project3_main_service.models.dto.AccountDto;
+import com.nourish1709.project3_main_service.exceptions.*;
+import com.nourish1709.project3_main_service.models.Account;
 
 public interface AccountInterface {
-    AccountDto update(Long id, AccountDto accountDto);
 
-    void setNotifications(Boolean enabledNotifications);
+    Account update(Account account)
+            throws AccountNotFoundException,
+            BadCredentialsException,
+            InvalidNameException,
+            InvalidPhoneException,
+            InvalidAgeException,
+            PhoneNumberIsAlreadyTakenException;
 
-    AccountDto getById(Long id);
+    void setNotifications(Long id, boolean enabledNotifications)
+            throws AccountNotFoundException;
+
+    Account getById(Long id)
+            throws AccountNotFoundException;
 }
