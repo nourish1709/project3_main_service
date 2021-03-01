f02227697a07d24f975aaf2b65da2c2c0026cd47
Barburon <lamis.htc@gmail.com>
2021-03-01 22:27:38 +0200

    Added ResponseEntity's

--- src/main/java/com/nourish1709/project3_main_service/controllers/AccountController.java
+++ src/main/java/com/nourish1709/project3_main_service/controllers/AccountController.java
@@ -2,6 +2,8 @@ package com.nourish1709.project3_main_service.controllers;
 
 import com.nourish1709.project3_main_service.models.dto.AccountDto;
 import com.nourish1709.project3_main_service.services.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,17 +17,32 @@ public class AccountController {
     }
 
     @PostMapping("/{id}")
-    public AccountDto updateAccount(@PathVariable Long id, @RequestBody AccountDto accountDto) {
-        return service.update(id, accountDto);
+    public ResponseEntity<AccountDto> updateAccount(@PathVariable Long id, @RequestBody AccountDto accountDto) {
+        AccountDto accountDto1 = new AccountDto();
+        accountDto1.setFirstName(accountDto.getFirstName());
+        accountDto1.setLastName(accountDto.getLastName());
+        accountDto1.setAge(accountDto.getAge());
+        accountDto1.setUser(accountDto.getUser());
+        accountDto1.setEnabledNotifications(accountDto.isEnabledNotifications());
+        AccountDto result = service.update(id, accountDto);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @PostMapping("/{id}/{notification}")
-    public void setNotification(@PathVariable Long id, @PathVariable boolean notification) {
+    public ResponseEntity<AccountDto> setNotification(@PathVariable Long id, @PathVariable boolean notification) {
+        AccountDto accountDto = service.getById(id);
+        accountDto.setEnabledNotifications(notification);
         service.setNotifications(id, notification);
+        return new ResponseEntity<>(accountDto, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public AccountDto getById(@PathVariable Long id){
-        return service.getById(id);
+    public ResponseEntity<AccountDto> findById(@PathVariable Long id) {
+        AccountDto accountDto = service.getById(id);
+        if (accountDto == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+
     }
 }
--- src/main/java/com/nourish1709/project3_main_service/controllers/SkuController.java
+++ src/main/java/com/nourish1709/project3_main_service/controllers/SkuController.java
@@ -2,6 +2,8 @@ package com.nourish1709.project3_main_service.controllers;
 
 import com.nourish1709.project3_main_service.models.dto.SkuDto;
 import com.nourish1709.project3_main_service.services.SkuService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,28 +19,54 @@ public class SkuController {
     }
 
     @PostMapping("/create")
-    public SkuDto createSku(@RequestBody SkuDto sku) {
-        return service.create(sku);
+    public ResponseEntity<SkuDto> createSku(@RequestBody SkuDto sku) {
+        SkuDto newSku = new SkuDto();
+        newSku.setName(sku.getName());
+        newSku.setPrice(sku.getPrice());
+        newSku.setDescription(sku.getDescription());
+        newSku.setAvailability(sku.isAvailability());
+        newSku.setCategoryId(sku.getCategoryId());
+        newSku.setAccountId(sku.getAccountId());
+        service.create(newSku);
+        return new ResponseEntity<>(newSku, HttpStatus.OK);
     }
 
     @GetMapping
-    public List<SkuDto> getAll() {
-        return service.getAll();
+    public ResponseEntity<List<SkuDto>> getAll() {
+        return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public SkuDto findById(@PathVariable Long id) {
-        return service.getById(id);
+    public ResponseEntity<SkuDto> findById(@PathVariable Long id) {
+        SkuDto sku = service.getById(id);
+        if (sku == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(sku, HttpStatus.OK);
     }
 
     @PostMapping("/{id}")
-    public SkuDto updateSku(@PathVariable Long id, @RequestBody SkuDto sku) {
-        return service.update(id, sku);
+    public ResponseEntity<SkuDto> updateSku(@PathVariable Long id, @RequestBody SkuDto sku) {
+        SkuDto newSku = service.getById(id);
+        newSku.setName(sku.getName());
+        newSku.setPrice(sku.getPrice());
+        newSku.setDescription(sku.getDescription());
+        newSku.setAvailability(sku.isAvailability());
+        newSku.setCategoryId(sku.getCategoryId());
+        newSku.setAccountId(sku.getAccountId());
+        SkuDto result = service.update(id, newSku);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteSku(@PathVariable Long id) {
+    public ResponseEntity<SkuDto> deleteSku(@PathVariable Long id) {
+        SkuDto sku = service.getById(id);
+        if (sku == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
         service.delete(id);
+        return new ResponseEntity<>(sku, HttpStatus.OK);
     }
-
 }
+
+
