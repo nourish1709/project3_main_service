87e58ae2b761c89b68e6a47b5f7266b07a01a945
zhenia <zhenya.savonenko944@gmail.com>
2021-03-02 23:23:20 +0200

    security added

---
+++ src/main/java/com/nourish1709/project3_main_service/security/entity/User.java
@@ -0,0 +1,65 @@
+package com.nourish1709.project3_main_service.security.entity;
+
+
+import com.nourish1709.project3_main_service.security.entity.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getGrantedAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/nourish1709/project3_main_service/services/UserService.java
@@ -0,0 +1,78 @@
+package com.nourish1709.project3_main_service.services;
+
+import com.nourish1709.project3_main_service.daos.UserRepository;
+import com.nourish1709.project3_main_service.security.entity.User;
+import com.nourish1709.project3_main_service.exceptions.IncorrectCredentialsException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.nourish1709.project3_main_service.security.entity.enums.UserRole.*;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private static final String SECRET = "secret";
+    private static final String EMAIL = "email";
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(IncorrectCredentialsException::new);
+    }
+
+    @PostConstruct
+    private void addUsers() {
+        List<User> users = Arrays.asList(
+                new User(
+                        1L,
+                        "Oleg",
+                        passwordEncoder.encode(SECRET),
+                        EMAIL,
+                        ADMIN
+                ),
+                new User(
+                        2L,
+                        "Roman",
+                        passwordEncoder.encode(SECRET),
+                        EMAIL,
+                        ADMIN
+                ),
+                new User(
+                        3L,
+                        "Andriana",
+                        passwordEncoder.encode(SECRET),
+                        EMAIL,
+                        ADMIN
+                ),
+                new User(
+                        4L,
+                        "zhenya",
+                        passwordEncoder.encode(SECRET),
+                        EMAIL,
+                        ADMIN
+                ),
+                new User(
+                        5L,
+                        "user",
+                        passwordEncoder.encode(SECRET),
+                        EMAIL,
+                        USER
+                )
+        );
+
+        userRepository.saveAll(users);
+    }
+}
